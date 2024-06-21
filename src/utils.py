14155bae40d661070db2bc3bea107d8be641f95b
Александр Рязанцеа <ryasantsev.a.r@gmail.com>
2024-06-21 11:16:21 +0300

    Fixes

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,4 @@
-
-
- #Вход функции
-# [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-
+from typing import Iterable
 
 
 def filter_by_state(list_of_data: Iterable[list[dict[str, str | int]]], condiyion: str="EXECUTED") -> Iterable[list[dict[str, str | int]]]:
@@ -14,9 +10,8 @@ def filter_by_state(list_of_data: Iterable[list[dict[str, str | int]]], condiyio
     return filtered_list_EXECUTED
 
 
-
 # 2
 
 def filter_by_date(filter_by_state: list, descending: bool = True) -> list:
     """Фильтрует транзакции по дате"""
-    return sorted(filter_by_state, key=lambda date: date.get("date"), reverse=descending
+    return sorted(filter_by_state, key=lambda date: date.get("date"), reverse=descending)
