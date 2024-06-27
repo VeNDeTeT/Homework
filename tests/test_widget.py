5210d2dcaf15d46d46b19f0ee640ede62939a679
Александр Рязанцеа <ryasantsev.a.r@gmail.com>
2024-06-27 16:16:16 +0300

    тест на проверку widget

---
+++ tests/test_widget.py
@@ -0,0 +1,19 @@
+import pytest
+
+from src.widget import mask_card_and_account
+
+@pytest.mark.parametrize(
+    "acc_number, expected",
+    [
+        ("Visa Platinum 7000 7922 8960 6361", "Visa Platinum 7000 79** **** 6361"),
+        ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+        ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
+        ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
+        ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
+        ("Счет 64686473678894779589", "Счет **9589"),
+        ("Счет 35383033474447895560", "Счет **5560"),
+        ("Счет 73654108430135874305", "Счет **4305"),
+    ],
+)
+def test_mask_card_and_account(acc_number, expected):
+    assert mask_card_and_account(acc_number) == expected
