3e2f1dff6a61a843540ad6eb4969901b4b938a6f
Александр Рязанцеа <ryasantsev.a.r@gmail.com>
2024-07-12 19:33:41 +0300

    путь до JSON-файла

---
+++ src/utils.py
@@ -0,0 +1,18 @@
+import json
+from typing import Any
+
+
+
+def get_transactions_dictionary(path: str) -> Any:
+    """Принимает путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях"""
+    try:
+        with open(path, "r", encoding="utf-8") as operations:
+            try:
+                transactions_data = json.load(operations)
+                return transactions_data
+            except json.JSONDecodeError:
+                transactions_data = []
+                return transactions_data
+    except FileNotFoundError:
+        transactions_data = []
+        return transactions_data
