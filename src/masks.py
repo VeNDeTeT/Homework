50cb6375511a37f232ee3399c6e943fa0eff49a3
Александр Рязанцеа <ryasantsev.a.r@gmail.com>
2024-08-17 11:25:18 +0300

    для модуля masks, настройка file_handler и file_formatter

--- src/masks.py
+++ src/masks.py
@@ -1,3 +1,23 @@
+import logging
+import os
+
+# Получаем абсолютный путь до текущей директории
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Создаем путь до файла логов относительно текущей директории
+rel_file_path = os.path.join(current_dir, "../logs/masks.log")
+abs_file_path = os.path.abspath(rel_file_path)
+
+# Добавляем логгер, который записывает логи в файл.
+logger = logging.getLogger("masks")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(abs_file_path, "w", encoding="utf-8")
+file_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s: %(message)s"
+)
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
 def get_mask_card_number(card_number: str) -> str:
     """
     принимает на вход номер карты и возвращает ее маску
