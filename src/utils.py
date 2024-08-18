9646439806908fd3cb9f1813ad93b74571f21818
VeNDeTeT <ryasantsev.a.r@gmail.com>
2024-08-18 10:07:22 +0300

    Feature/homework 12.2 (#19)
    * для модуля utils, настройка
    file_handler и file_formatter
    
    * для модуля masks, настройка
    file_handler и file_formatter
    
    * Исправления
    
    * Описание

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,28 @@
 import json
+import logging
+import os
 from typing import Any
+
+# Получаем абсолютный путь до текущей директории
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Создаем путь до файла логов относительно текущей директории
+rel_log_file_path = os.path.join(current_dir, "../logs/utils.log")
+abs_log_file_path = os.path.abspath(rel_log_file_path)
+
+# Создаем путь до файла JSON относительно текущей директории
+rel_src_file_path = os.path.join(current_dir, "../data/operations.json")
+abs_src_file_path = os.path.abspath(rel_src_file_path)
+
+# Добавляем логгер, который записывает логи в файл.
+logger = logging.getLogger("utils")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(abs_log_file_path, "w", encoding="utf-8")
+file_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s: %(message)s"
+)
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def get_info(file_json: str) -> Any:
@@ -16,6 +39,5 @@ def get_info(file_json: str) -> Any:
             return []
         except json.JSONDecodeError:
             return []
-
 
 
